63e094b465eb2e8edf3ad21185bbdddd1d1fddb0
Mark Koshelev <markukoshelev@gmail.com>
2022-09-22 13:38:56 +0300

    cosmeticz

--- tasmota/user_config_override.h
+++ tasmota/user_config_override.h
@@ -3,9 +3,6 @@
 https://blog.sitefreeman.com/2020/04/03/%D1%82%D1%80%D0%B5%D1%85%D1%84%D0%B0%D0%B7%D0%BD%D1%8B%D0%B9-wi-fi-%D1%81%D1%87%D0%B5%D1%82%D1%87%D0%B8%D0%BA-dds6619-016-017-sinotimer-rs485-tx-rx-modbus-sonoff-basic-majordomo/
 
 
-
-
-
 50:0F:F5:C0:2A:C0	 192.168.1.40	 TENDA-REPEATER	
 04:95:E6:E0:5E:40	 192.168.1.41	 IoT-MM
 
